33ee7d1dcc1fb77c8b7c25c6cb1bef31c37d2d4f
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-09-30 14:33:57 +0700

    Revert "challenge_fix"
    This reverts commit 544a58e1e519b44933a500157443fc9c7b578d84.

--- app/src/main/java/jti/annisafitriy/a04androidrecyclerview/MainActivity.java
+++ app/src/main/java/jti/annisafitriy/a04androidrecyclerview/MainActivity.java
@@ -1,22 +1,22 @@
 package jti.annisafitriy.a04androidrecyclerview;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.LinkedList;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private final LinkedList<String> mWordList = new LinkedList<>();
     private RecyclerView mRecyclerView;
-
+    private WordListAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +25,34 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
-            int wordListSize = mWordList.size();
-            mWordList.addLast("+ Word " + wordListSize);
-            Objects.requireNonNull(mRecyclerView.getAdapter()).notifyItemInserted(wordListSize);
-            mRecyclerView.smoothScrollToPosition(wordListSize);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+                mWordList.addLast("+ Word " + wordListSize);
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
         });
 
         for (int i = 0; i < 20; i++) {
             mWordList.addLast("Word " + i);
         }
         mRecyclerView = findViewById(R.id.recyclerview);
-        WordListAdapter mAdapter = new WordListAdapter(this, mWordList);
+        mAdapter = new WordListAdapter(this, mWordList);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FloatingActionButton fab2 = findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWordList.clear();
+                for (int i = 0; i < 20; i++) {
+                    mWordList.addLast("Word " + i);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -48,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @SuppressLint("NotifyDataSetChanged")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -57,12 +70,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.reset) {
-            mWordList.clear();
-            for (int i = 0; i < 20; i++){
-                mWordList.addLast("Word " + i);
-            }
-            Objects.requireNonNull(mRecyclerView.getAdapter()).notifyDataSetChanged();
+        if (id == R.id.action_settings) {
             return true;
         }
 
